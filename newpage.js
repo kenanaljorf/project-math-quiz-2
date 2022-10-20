f47efb775517d2c50ece781762aadc02248b45ee
kenanaljorf <101208540+kenanaljorf@users.noreply.github.com>
2022-10-20 00:44:00 -0500

    Add files via upload

---
+++ newpage.js
@@ -0,0 +1,5 @@
+document.getElementById("player1_name").innerHTML =  player1_name;
+document.getElementById("player2_name").innerHTML =  player2_name;
+
+document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
+document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
